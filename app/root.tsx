ecef72088221fc1ab1612cf7dd9562fa3abeabc0
Ben Vinegar <ben@benv.ca>
2024-01-01 15:49:16 -0500

    Add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,7 @@ export default function App() {
             <head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" type="image/x-icon" href="/favicon.png" />
                 <Meta />
                 <Links />
             </head>
