c6701778016089c06abdbdcecf8536f31f476669
Ben Vinegar <ben@benv.ca>
2024-01-31 19:46:02 -0500

    Move column mappings to query.ts

---
+++ app/analytics/schema.ts
@@ -0,0 +1,17 @@
+export interface ColumnMappingsType {
+    [key: string]: any
+}
+
+export const ColumnMappings: ColumnMappingsType = {
+    host: "blob1",
+    userAgent: "blob2",
+    path: "blob3",
+    country: "blob4",
+    referrer: "blob5",
+    browserName: "blob6",
+    deviceModel: "blob7",
+    siteId: "blob8",
+
+    newVisitor: "double1",
+    newSession: "double2",
+};
