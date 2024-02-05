c0a6f0af123ae7e707119c7bdb289c9c0637ff74
Alex Chiu <alexchiu11@gmail.com>
2024-02-05 10:36:21 +0000

    feat: implements bar charts

--- app/components/TableCard.tsx
+++ app/components/TableCard.tsx
@@ -11,15 +11,33 @@ import {
 
 import { Card } from "~/components/ui/card";
 
+type CountByProperty = [string, string][];
+
+function calculateCountPercentages(countByProperty: CountByProperty) {
+    const totalCount = countByProperty.reduce(
+        (sum, row) => sum + parseInt(row[1]),
+        0
+    );
+
+    return countByProperty.map((row) => {
+        const count = parseInt(row[1]);
+        const percentage = ((count / totalCount) * 100).toFixed(2);
+        return `${percentage}%`;
+    });
+}
 export default function TableCard({
     countByProperty,
     columnHeaders,
 }: InferProps<typeof TableCard.propTypes>) {
+    const barChartPercentages = calculateCountPercentages(
+        countByProperty as CountByProperty
+    );
     return (
         <Card>
             <Table>
                 <TableHeader>
                     <TableRow>
+                        <th />
                         {(columnHeaders || []).map((header: string, index) => (
                             <TableHead
                                 key={header}
@@ -33,12 +51,21 @@ export default function TableCard({
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {(countByProperty || []).map((item: any) => (
-                        <TableRow key={item[0]}>
-                            <TableCell className="font-medium">
+                    {(countByProperty || []).map((item: any, key) => (
+                        <TableRow
+                            key={item[0]}
+                            className="relative group [&_div]:last:rounded-b-md"
+                        >
+                            <div
+                                className="bg-orange-200 absolute h-full after:content-[''] group-hover:opacity-50"
+                                style={{
+                                    width: barChartPercentages[key],
+                                }}
+                            />
+                            <TableCell className="font-medium relative">
                                 {item[0]}
                             </TableCell>
-                            <TableCell className="text-right">
+                            <TableCell className="text-right relative">
                                 {item[1]}
                             </TableCell>
                         </TableRow>
