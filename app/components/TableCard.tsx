ca533fb6161c2ede8b1aff1187e58e56885a7990
Alex Chiu <alexchiu11@gmail.com>
2024-02-08 05:21:03 +0000

    feat: bar charts  (#39)
    * feat: implements bar charts
    
    * fix hydration errors causing select to break
    
    * fix: comment typo
    
    ---------
    
    Co-authored-by: Ben Vinegar <2153+benvinegar@users.noreply.github.com>

--- app/components/TableCard.tsx
+++ app/components/TableCard.tsx
@@ -11,15 +11,34 @@ import {
 
 import { Card } from "~/components/ui/card";
 
+type CountByProperty = [string, string][];
+
+function calculateCountPercentages(countByProperty: CountByProperty) {
+    const totalCount = countByProperty.reduce(
+        (sum, row) => sum + parseInt(row[1]),
+        0
+    );
+
+    return countByProperty.map((row) => {
+        const count = parseInt(row[1]);
+        const percentage = ((count / totalCount) * 100).toFixed(2);
+        return `${percentage}%`;
+    });
+}
 export default function TableCard({
     countByProperty,
     columnHeaders,
 }: InferProps<typeof TableCard.propTypes>) {
+    const barChartPercentages = calculateCountPercentages(
+        countByProperty as CountByProperty
+    );
     return (
         <Card>
             <Table>
                 <TableHeader>
                     <TableRow>
+                        {/* empty header required otherwise column for the bar chart wont render */}
+                        <th />
                         {(columnHeaders || []).map((header: string, index) => (
                             <TableHead
                                 key={header}
@@ -33,12 +52,22 @@ export default function TableCard({
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {(countByProperty || []).map((item) => (
-                        <TableRow key={item[0]}>
-                            <TableCell className="font-medium">
+                    {(countByProperty || []).map((item, key) => (
+                        <TableRow
+                            key={item[0]}
+                            className="relative group [&_td]:last:rounded-b-md"
+                        >
+                            {/* element _must_ be a `td` otherwise we get hydration errors */}
+                            <td
+                                className="bg-orange-200 absolute h-full after:content-[''] group-hover:opacity-50"
+                                style={{
+                                    width: barChartPercentages[key],
+                                }}
+                            />
+                            <TableCell className="font-medium relative">
                                 {item[0]}
                             </TableCell>
-                            <TableCell className="text-right">
+                            <TableCell className="text-right relative">
                                 {item[1]}
                             </TableCell>
                         </TableRow>
