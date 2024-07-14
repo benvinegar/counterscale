47605af0ba890a7f9ad3c5bbef723198c1c19432
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-07-14 11:21:12 -0400

    Separate root and layout components so tests pass

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import {
     Outlet,
     Scripts,
     ScrollRestoration,
+    useLoaderData,
     useRouteError,
     useRouteLoaderData,
 } from "@remix-run/react";
@@ -73,61 +74,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
                 <Links />
             </head>
             <body>
-                <div className="container mx-auto mt-4">
-                    <header className="border-b-2 mb-12 py-4">
-                        <nav className="flex justify-between items-center">
-                            <div className="flex items-center">
-                                <a
-                                    href="/"
-                                    className="text-xl sm:text-2xl font-bold"
-                                >
-                                    Counterscale
-                                </a>
-                                <img
-                                    className="w-6 sm:w-8 ml-1"
-                                    src="/favicon.png"
-                                    alt="Counterscale Icon"
-                                />
-                            </div>
-                            <div className="flex items-center font-small font-medium text-md sm:text-lg">
-                                <a href="/dashboard">Dashboard</a>
-                                <a
-                                    href="/admin-redirect"
-                                    target="_blank"
-                                    className="hidden sm:inline-block ml-2 sm:ml-4"
-                                >
-                                    Admin
-                                </a>
-                                <a
-                                    href="https://github.com/benvinegar/counterscale"
-                                    className="w-8 ml-2 sm:ml-4"
-                                >
-                                    <img
-                                        src="/github-mark.svg"
-                                        alt="GitHub Logo"
-                                        style={{
-                                            filter: "invert(21%) sepia(27%) saturate(271%) hue-rotate(113deg) brightness(97%) contrast(97%)",
-                                        }}
-                                    />
-                                </a>
-                            </div>
-                        </nav>
-                    </header>
-                    <main role="main" className="w-full">
-                        {children}
-                    </main>
-
-                    <footer className="py-4 flex justify-end text-s">
-                        <div>
-                            Version{" "}
-                            <a
-                                href={`https://github.com/benvinegar/counterscale/commit/${data.version}`}
-                            >
-                                {data.version?.slice(0, 7)}
-                            </a>
-                        </div>
-                    </footer>
-                </div>
+                <div className="container mx-auto mt-4">{children}</div>
                 <ScrollRestoration />
                 <Scripts />
                 <script
@@ -142,7 +89,61 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default function App() {
-    return <Outlet />;
+    const data = useLoaderData<typeof loader>();
+    return (
+        <div>
+            <header className="border-b-2 mb-12 py-4">
+                <nav className="flex justify-between items-center">
+                    <div className="flex items-center">
+                        <a href="/" className="text-xl sm:text-2xl font-bold">
+                            Counterscale
+                        </a>
+                        <img
+                            className="w-6 sm:w-8 ml-1"
+                            src="/favicon.png"
+                            alt="Counterscale Icon"
+                        />
+                    </div>
+                    <div className="flex items-center font-small font-medium text-md sm:text-lg">
+                        <a href="/dashboard">Dashboard</a>
+                        <a
+                            href="/admin-redirect"
+                            target="_blank"
+                            className="hidden sm:inline-block ml-2 sm:ml-4"
+                        >
+                            Admin
+                        </a>
+                        <a
+                            href="https://github.com/benvinegar/counterscale"
+                            className="w-8 ml-2 sm:ml-4"
+                        >
+                            <img
+                                src="/github-mark.svg"
+                                alt="GitHub Logo"
+                                style={{
+                                    filter: "invert(21%) sepia(27%) saturate(271%) hue-rotate(113deg) brightness(97%) contrast(97%)",
+                                }}
+                            />
+                        </a>
+                    </div>
+                </nav>
+            </header>
+            <main role="main" className="w-full">
+                <Outlet />;
+            </main>
+
+            <footer className="py-4 flex justify-end text-s">
+                <div>
+                    Version{" "}
+                    <a
+                        href={`https://github.com/benvinegar/counterscale/commit/${data.version}`}
+                    >
+                        {data.version?.slice(0, 7)}
+                    </a>
+                </div>
+            </footer>
+        </div>
+    );
 }
 
 export const ErrorBoundary = () => {
