215f4f495256c6c62c585e9522b5b73f738b34e7
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-07-31 22:27:33 -0400

    Expand utils.test.ts coverage

--- app/lib/utils.test.ts
+++ app/lib/utils.test.ts
@@ -5,15 +5,19 @@ import { describe, test, expect } from "vitest";
 describe("getFiltersFromSearchParams", () => {
     test("it should return an object with the correct keys", () => {
         const searchParams = new URLSearchParams(
-            "?path=/about&referrer=google.com",
+            "?path=/about&referrer=google.com&deviceModel=iphone&country=us&browserName=chrome",
         );
         expect(getFiltersFromSearchParams(searchParams)).toEqual({
             path: "/about",
             referrer: "google.com",
+            deviceModel: "iphone",
+            country: "us",
+            browserName: "chrome",
         });
     });
 
     test("it should ignore keys it doesn't recognize", () => {
+        // only path is valid; unknown should be discarded
         const searchParams = new URLSearchParams("?unknown=foo&path=/about");
         expect(getFiltersFromSearchParams(searchParams)).toEqual({
             path: "/about",
