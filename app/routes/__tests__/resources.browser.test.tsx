4482d6a1d79d148f203976aaa830ffcdd716482a
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-08-12 22:00:09 -0400

    Add tests for browser and device resource routes

---
+++ app/routes/__tests__/resources.browser.test.tsx
@@ -0,0 +1,59 @@
+// @vitest-environment jsdom
+import {
+    vi,
+    test,
+    describe,
+    beforeEach,
+    afterEach,
+    expect,
+    Mock,
+} from "vitest";
+import "vitest-dom/extend-expect";
+
+import { loader } from "../resources.browser";
+import { createFetchResponse, getDefaultContext } from "./testutils";
+
+describe("Resources/Browser route", () => {
+    let fetch: Mock;
+
+    beforeEach(() => {
+        fetch = global.fetch = vi.fn();
+    });
+
+    afterEach(() => {
+        vi.restoreAllMocks();
+    });
+
+    describe("loader", () => {
+        test("returns valid json", async () => {
+            fetch.mockResolvedValueOnce(
+                createFetchResponse({
+                    data: [
+                        { blob6: "Chrome", count: "5" },
+                        { blob6: "Firefox", count: "1" },
+                    ],
+                }),
+            );
+
+            const response = await loader({
+                ...getDefaultContext(),
+                // @ts-expect-error we don't need to provide all the properties of the request object
+                request: {
+                    url: "http://localhost:3000/resources/browser", // no site query param
+                },
+            });
+
+            // expect redirect
+            expect(response.status).toBe(200);
+
+            const json = await response.json();
+            expect(json).toEqual({
+                countsByProperty: [
+                    ["Chrome", 5],
+                    ["Firefox", 1],
+                ],
+                page: 1,
+            });
+        });
+    });
+});
