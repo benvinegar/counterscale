53d58356288cc70674d2057053f6c38883c837ad
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-12-13 13:13:05 -0500

    Convert tailwind config to TS (fixes build issues in newer versions of Node)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
     darkMode: ["class"],
     content: [
         "./pages/**/*.{ts,tsx}",
@@ -82,4 +83,4 @@ module.exports = {
         },
     },
     plugins: [require("tailwindcss-animate")],
-};
+} satisfies Config;
