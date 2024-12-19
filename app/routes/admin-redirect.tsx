32d3de51e72ae1e9b07ec9a3f696bbd90dc60fc0
Brett Hoerner <brett@bretthoerner.com>
2024-12-19 06:17:52 -0700

    fix admin url to use Pages

--- app/routes/admin-redirect.tsx
+++ app/routes/admin-redirect.tsx
@@ -2,6 +2,6 @@ import { LoaderFunctionArgs, redirect } from "@remix-run/cloudflare";
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
     return redirect(
-        `https://dash.cloudflare.com/${context.cloudflare.env.CF_ACCOUNT_ID}/workers/services/view/counterscale/production`,
+        `https://dash.cloudflare.com/${context.cloudflare.env.CF_ACCOUNT_ID}/pages/view/counterscale`,
     );
 };
