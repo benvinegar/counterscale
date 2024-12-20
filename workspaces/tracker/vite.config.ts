51dbdb2696fc3f3c91108fda351f8dedd50974e2
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-12-20 17:04:28 -0500

    Convert tracker to typescript

--- workspaces/tracker/vite.config.ts
+++ workspaces/tracker/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/index.js",
-      name: "tracker",
+    build: {
+        lib: {
+            entry: "src/index.ts",
+            name: "tracker",
+        },
     },
-  },
+    plugins: [
+        {
+            name: "wrap-in-iife",
+            generateBundle(outputOptions, bundle) {
+                Object.keys(bundle).forEach((fileName) => {
+                    const file = bundle[fileName];
+                    if (fileName.slice(-3) === ".js" && "code" in file) {
+                        file.code = `(() => {\n${file.code}})()`;
+                    }
+                });
+            },
+        },
+    ],
 });
