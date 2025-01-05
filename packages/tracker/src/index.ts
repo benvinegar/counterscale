c6de896a2d7900d0eb6080233f3f2bc25e4bdbf9
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2025-01-05 13:46:52 -0500

    Preserve pushState arity

--- packages/tracker/src/index.ts
+++ packages/tracker/src/index.ts
@@ -80,8 +80,15 @@ function findReporterScript() {
 function instrument() {
     const origPushState = history.pushState;
 
-    history.pushState = function (data, title, url) {
-        origPushState.apply(this, [data, title, url]);
+    // NOTE: Intentionally only declaring 2 parameters for this pushState wrapper,
+    //       because that is the arity of the built-in function we're overwriting.
+
+    // See: https://blog.sentry.io/wrap-javascript-functions/#preserve-arity
+
+    // eslint-disable-next-line
+    history.pushState = function (data, title /*, url */) {
+        // eslint-disable-next-line
+        origPushState.apply(this, arguments as any);
         trackPageview({});
     };
 }
