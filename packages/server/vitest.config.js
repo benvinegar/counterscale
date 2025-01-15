1c544cd2f9ade63e7261803cd84566ae3d1cfbf1
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2025-01-15 14:22:14 -0500

    Remove build folder, Tailwind config from coverage report

--- packages/server/vitest.config.js
+++ packages/server/vitest.config.js
@@ -1,11 +1,17 @@
 // vitest.config.ts
-import { defineConfig } from "vitest/config";
+import { defineConfig, coverageConfigDefaults } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     test: {
         coverage: {
             provider: "v8", // or 'v8'
+            exclude: [
+                "build",
+                "tailwind.config.ts",
+                "public/tracker.js",
+                ...coverageConfigDefaults.exclude,
+            ],
         },
     },
     plugins: [tsconfigPaths()],
