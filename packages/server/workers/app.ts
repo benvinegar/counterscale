d94fc29741e479d3918d89d6976c46569cba3d67
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2025-01-16 16:08:56 -0500

    Migrate back to Cloudflare Workers (#147)
    * Migrate back to Cloudflare Workers
    
    * Delete deprecated CF pages files

---
+++ packages/server/workers/app.ts
@@ -0,0 +1,38 @@
+import type { ExportedHandler } from "@cloudflare/workers-types";
+import { createRequestHandler, type ServerBuild } from "react-router";
+
+import { getLoadContext } from "~/load-context";
+
+import * as build from "../build/server";
+
+const requestHandler = createRequestHandler(build as unknown as ServerBuild);
+
+export default {
+    // @ts-expect-error TODO figure out types here
+    async fetch(request: any, env: any, ctx: any) {
+        try {
+            const loadContext = getLoadContext({
+                request,
+                context: {
+                    cloudflare: {
+                        ctx: {
+                            waitUntil: ctx.waitUntil.bind(ctx),
+                            passThroughOnException:
+                                ctx.passThroughOnException.bind(ctx),
+                        },
+                        cf: request.cf as never,
+                        // @ts-expect-error TODO: figure out how to get this type to work
+                        caches,
+                        env,
+                    },
+                },
+            });
+            return await requestHandler(request, loadContext);
+        } catch (error) {
+            console.log(error);
+            return new Response("An unexpected error occurred", {
+                status: 500,
+            });
+        }
+    },
+} satisfies ExportedHandler<Env>;
